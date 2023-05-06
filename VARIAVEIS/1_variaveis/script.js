6ff3f25f847d6266b4cf508a6619c6b8feb3955e
Cleidson Fias <cleidson.fias@hotmail.com>
2023-05-06 12:44:51 -0300

    Variáveis
    let

---
+++ VARIAVEIS/1_variaveis/script.js
@@ -0,0 +1,13 @@
+let nome = "Cleidson Fias";
+console.log(nome);
+console.log(`O meu nome é ${nome}.`);
+
+let laranjas = 5;
+console.log(`Eu tenho ${laranjas} laranjas.`)
+console.log(laranjas * laranjas);
+
+nome = "Milena Santana";
+console.log(nome);
+
+laranjas = "Laranja";
+console.log(laranjas);
