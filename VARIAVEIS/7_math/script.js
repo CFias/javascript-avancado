2d262908c373c530de71f6b77c514c81a5bea549
Cleidson Fias <cleidson.fias@hotmail.com>
2023-05-06 14:07:07 -0300

    math
    praticas com o math

---
+++ VARIAVEIS/7_math/script.js
@@ -0,0 +1,11 @@
+let maior = Math.max(5, 10, 1899, 40);
+console.log(maior);
+
+let menor = Math.min(5, 10, 1899, 40);
+console.log(menor);
+
+let arredondar = Math.round(5.4341231);
+console.log(arredondar);
+
+let arredondarParaCima = Math.ceil(5.4341231);
+console.log(arredondarParaCima);
