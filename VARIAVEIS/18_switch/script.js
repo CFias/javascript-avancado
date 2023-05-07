04d4aa1a19c24348f850067d3216682edb1f9fe6
Cleidson Fias <cleidson.fias@hotmail.com>
2023-05-07 15:41:56 -0300

    switch
    Trabalhando com switch em JS

---
+++ VARIAVEIS/18_switch/script.js
@@ -0,0 +1,15 @@
+let nome = prompt("Qual é o seu nome ?");
+
+switch(nome) {
+    case "Cleidson Fias":
+        console.log("O nome é Cleidson Fias!");
+        break;
+
+    case "Milena Santana":
+        console.log("O nome é Milena Santana!");
+        break;
+
+    default:
+        console.log("O nome não foi encontrado!");
+        break;
+}
