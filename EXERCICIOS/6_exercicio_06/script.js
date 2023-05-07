c3042111b13097c8585cd72bff71a8087e962fda
Cleidson Fias <cleidson.fias@hotmail.com>
2023-05-07 15:46:30 -0300

    Tarefas
    Pondo em prática o aprendizado

---
+++ EXERCICIOS/6_exercicio_06/script.js
@@ -0,0 +1,7 @@
+let nome = "Cleidson Fias";
+let num = 25;
+let boo = true;
+
+console.log(typeof nome);
+console.log(typeof num);
+console.log(typeof boo);
---
+++ EXERCICIOS/8_exercicio_08/script.js
@@ -0,0 +1,5 @@
+const nome = "Cleidson Fias";
+
+if (nome == "Cleidson Fias") {
+    console.log(`Saudações, ${nome}!`)
+}
