d9dce44eda87cf22f6788ebcd65376c1fcf418bd
Cleidson Fias <cleidson.fias@hotmail.com>
2023-05-07 15:44:22 -0300

    Estrutura for
    Estrutura de repetição For

---
+++ VARIAVEIS/14_for/script.js
@@ -0,0 +1,7 @@
+for (let num = 0; num < 100; num = num + 3) {
+    console.log(`O resultado da soma de num + 2 é: ${num + 2}`)
+}
+
+for (let velocidade = 0; velocidade <= 80; velocidade = velocidade + 1) {
+    console.log(`${velocidade}km foi a sua velocidade`)
+}
