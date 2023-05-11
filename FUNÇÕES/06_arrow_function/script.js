d7ccd6247c96752c8af4ad17809436651abe77d1
Cleidson Fias <cleidson.fias@hotmail.com>
2023-05-11 12:59:22 -0300

    arrow function
    trabalhando com arrow function e entendo quando pode ser usada com boa prática

---
+++ FUNÇÕES/06_arrow_function/script.js
@@ -0,0 +1,11 @@
+let consoleTeste = () => {
+    console.log("Olá, mundo!");
+};
+
+consoleTeste();
+
+let somaComum = (a,b) => {
+    return a + b;
+};
+
+console.log(somaComum(10, 20));
---
+++ FUNÇÕES/07_arrow_function_2/script.js
@@ -0,0 +1,10 @@
+const concatenarNome = (nome) => {
+    return nome + "Fias";
+};
+
+console.log(concatenarNome("Cleidson"));
+
+
+const abreviandoCod = nom => nom + "Fias";
+
+console.log(abreviandoCod("Ramos"));
