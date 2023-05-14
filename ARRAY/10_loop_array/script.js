217c369d38a97f4c2358d22daabba498b1f1c778
Cleidson Fias <cleidson.fias@hotmail.com>
2023-05-14 13:09:02 -0300

    loop array
    Criando loop de array e entendendo no ambiente real de trabalho como funciona

---
+++ ARRAY/10_loop_array/script.js
@@ -0,0 +1,12 @@
+let nomes = ["Cleidson", "Milena", "Celia"];
+
+for (let i = 0; i <= nomes.length; i++) {
+    console.log(nomes[i]);
+};
+
+
+let casa = ["2 Quartos", "Sala", "Cozinha"];
+
+for (let i = 0; i <= casa.length; i++) {
+    console.log(casa[i]);
+};
