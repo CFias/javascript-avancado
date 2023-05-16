060949a428446da5dfdf4970c2b9181d2461724b
Cleidson Fias <cleidson.fias@hotmail.com>
2023-05-15 22:02:03 -0300

    Método push e pop
    Método que adiciona e apaga o último elemento de uma array

---
+++ ARRAY/11_push_pop/script.js
@@ -0,0 +1,12 @@
+let nomes = ['Cleidson', 'Milena', 'Claise'];
+
+console.log(nomes);
+
+let removerNome = nomes.pop();
+
+console.log(removerNome);
+console.log(nomes);
+
+nomes.push('Fias');
+
+console.log(nomes);
