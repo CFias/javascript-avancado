2ec91d0768f0dcec1cf8c0baa7f4ea9f247f8e29
Cleidson Fias <cleidson.fias@hotmail.com>
2023-05-16 00:33:19 -0300

    JSON
    Semelhante a um objeto, JSON é muito bem utilizado para interação entre back-end e front-end com seus dados

---
+++ ARRAY/26_json/script.js
@@ -0,0 +1,11 @@
+let pessoa = {
+    "nome": "Cleidson Fias",
+    "Idade": 24,
+    "Naturalidade": "Baiano",
+    "Email": "cleidson.fias@hotmail.com"
+};
+
+console.log(pessoa.nome);
+console.log(pessoa.Idade);
+console.log(pessoa.Naturalidade);
+console.log(pessoa.Email);
