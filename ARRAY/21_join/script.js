27001aa6a7a8afab4aee89c2e31ba2c2fe5d6b70
Cleidson Fias <cleidson.fias@hotmail.com>
2023-05-16 00:27:04 -0300

    Método join
    Método que permite transformar uma array em uma frase

---
+++ ARRAY/21_join/script.js
@@ -0,0 +1,13 @@
+let frase = "Testando o método split";
+
+let palavra = frase.split(" ");
+
+console.log(palavra);
+
+
+let metodoJoin = palavra.join("@");
+
+console.log(metodoJoin);
+console.log(palavra.join(" "));
+
+// Transformando array em frase
