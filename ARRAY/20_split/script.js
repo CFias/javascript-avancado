0cd942280f8cba3ddd6a85a5fffaa9b7ae2586eb
Cleidson Fias <cleidson.fias@hotmail.com>
2023-05-16 00:25:34 -0300

    Método split
    Método que permite transformar uma frase em array através de símbolos ou espaçamento

---
+++ ARRAY/20_split/script.js
@@ -0,0 +1,10 @@
+let frase = "Dando o melhor nas condições atuais";
+
+let transformandoArray = frase.split(" ");
+console.log(transformandoArray);
+
+
+let jogos = "Ponte Preta;ABC;Londrina;Botafogo-SP;Ceará;Mirassol";
+
+let jogosArray = jogos.split(';');
+console.log(jogosArray);
