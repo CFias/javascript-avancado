31ed8c63e1377615b31726eb0e16880b60566838
Cleidson Fias <cleidson.fias@hotmail.com>
2023-05-15 22:04:38 -0300

    Método indexOf e lastIndexOf
    Método que busca o elemento da array pelo valor passado

---
+++ ARRAY/13_indexof_lastindexof/script.js
@@ -0,0 +1,10 @@
+let num = [4,2,1,7,5,9,2];
+
+console.log(num.indexOf(2));
+console.log(num.lastIndexOf(2));
+
+
+let nomes = ['Cleidson', 'Ramos', 'Fias'];
+
+console.log(nomes.indexOf('Ramos'));
+console.log(nomes.lastIndexOf('Cleidson'));
