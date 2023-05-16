f10177743f51ccbdb3628ba770ddef64543c1081
Cleidson Fias <cleidson.fias@hotmail.com>
2023-05-15 22:03:04 -0300

    Método shift e unshift
    Método que adiciona e apaga o primeiro elemento da array

---
+++ ARRAY/12_shift_unshift/script.js
@@ -0,0 +1,12 @@
+let carros = ["Ford", "Chevrolet", "Toyota"];
+
+let elementoRemovido = carros.shift();
+
+console.log(elementoRemovido);
+console.log(carros);
+
+
+carros.unshift("Celta");
+
+console.log(carros);
+console.log(carros[0]);
