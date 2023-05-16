e00276e36d197050ffc17c5fd1241d2820981d1e
Cleidson Fias <cleidson.fias@hotmail.com>
2023-05-15 22:06:46 -0300

    Método slice
    Método que permite uma "navegação" pela array, com busca a partir de um valor e entre os valores ou até decrementando a lista com o sinal de negativo

---
+++ ARRAY/14_slice/script.js
@@ -0,0 +1,9 @@
+let nums = [0,1,2,3,4,5,6,7,8,9];
+
+console.log(nums.slice(7,9));
+console.log(nums.slice(5,6));
+
+console.log(nums.slice(3))
+
+console.log(nums.slice(-1));
+console.log(nums.slice(3,-3));
