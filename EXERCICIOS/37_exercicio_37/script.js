665b035aa3f17481413e1a044a80639e2bb094cf
Cleidson Fias <cleidson.fias@hotmail.com>
2023-05-22 23:06:59 -0300

    Tarefa de reforço
    Trabalhando com esquema de carrinho de e-commerce

--- EXERCICIOS/37_exercicio_37/script.js
+++ EXERCICIOS/37_exercicio_37/script.js
@@ -1,52 +1,72 @@
-class Compras {
-    constructor(itens, qtdTotal, valorTotal) {
+class Carrinho {
+    constructor(itens, qtd, valorTotal) {
         this.itens = itens;
-        this.qtdTotal = qtdTotal;
+        this.qtd = qtd;
         this.valorTotal = valorTotal;
     }
+
     addItem(item) {
-        for(let itemCarrinho in this.itens) {
-           if(this.itens[itemCarrinho].id == item.id) {
-            this.itens[itemCarrinho].qtd += item.qtd;
-           };
+
+        let contador = 0;
+
+        for (let itemCarrinho in this.itens) {
+            if (this.itens[itemCarrinho].id == item.id) {
+                this.itens[itemCarrinho].qtd += item.qtd;
+                contador = 1;
+            }
         }
+
+        if(contador == 0) {
+            this.itens.push(item);
+        }
+
         this.qtd += item.qtd;
         this.valorTotal += item.preco * item.qtd;
+
     }
 
     removeItem(item) {
         for(let itemCarrinho in this.itens) {
             if(this.itens[itemCarrinho].id == item.id) {
-                this.itens[itemCarrinho].qtd -= item.id
-            };
+
+                let obj = this.itens[itemCarrinho];
+                let index = this.itens.findIndex(function(obj) {
+                        return obj.id == item.id
+                    }
+                )
+                this.qtd -=  this.itens[itemCarrinho].qtd;
+                this.valorTotal -=  this.itens[itemCarrinho].preco *  this.itens[itemCarrinho].qtd;
+
+                this.itens.splice(index, 1);
+
+            }
         }
     }
+
 };
 
-let Carrinho = new Compras([
+let carrinho = new Carrinho ([
     {
         id: 01,
         nome: "Camisa",
-        qtd: 1,
+        qtd: 3,
         preco: 20
     },
     {
         id: 02,
         nome: "Calça",
         qtd: 2,
-        preco: 60
-    },
-    {
-        id: 03,
-        nome: "Casaco",
-        qtd: 4,
-        preco: 220
+        preco: 50
     }
-], 3, 300);
+],5, 160);
 
-console.log(Carrinho);
-Carrinho.addItem({id:01, nome:"Camisa", qtd:3, preco:35});
-console.log(Carrinho);
+console.log(carrinho);
 
+carrinho.addItem({id:01, nome:"Camisa", qtd:2, preco:20});
+console.log(carrinho);
 
+carrinho.addItem({id:03, nome:"Sapato", qtd:5, preco:110});
+console.log(carrinho);
 
+carrinho.removeItem({id:01, nome:"Camisa", qtd:02, preco:20});
+console.log(carrinho);
