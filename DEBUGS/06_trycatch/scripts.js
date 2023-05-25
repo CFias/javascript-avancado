b6acb6c362fe11ea67cba13a264127f7e8a7be69
Cleidson Fias <cleidson.fias@hotmail.com>
2023-05-25 00:53:54 -0300

    Debugando

---
+++ DEBUGS/06_trycatch/scripts.js
@@ -0,0 +1,13 @@
+let b = 2;
+
+try {
+
+    let a = 2 + b;
+
+} catch(error) {
+
+    console.log(error);
+  
+};
+
+console.log('Teste');
---
+++ DEBUGS/07_finally/scripts.js
@@ -0,0 +1,17 @@
+let b = 2;
+
+try {
+
+    let a = 2 + b;
+
+} catch(error) {
+
+    console.log(error);
+
+} finally {
+
+    console.log("Executou!")
+
+}
+
+console.log('Teste');
