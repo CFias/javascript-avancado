bd34695542db9579039903b3ead688788987e3fe
Cleidson Fias <cleidson.fias@hotmail.com>
2023-05-30 14:56:45 -0300

    appenChild

--- JS NAVEGADOR/07_appenChild/scripts.js
+++ JS NAVEGADOR/07_appenChild/scripts.js
@@ -1,5 +1,9 @@
-let p = document.createElement("p");
-let el2 = document.querySelector("h1");
-let pai2 = el2.parentNode;
+let novoElemento = document.createElement("p");
+let texto = document.createTextNode("Algum texto de teste");
 
-pai2.appendChild(p);
+novoElemento.appendChild(texto);
+
+let p = document.querySelector("#paragrafo-principal");
+let pai = p.parentNode;
+
+pai.appendChild(novoElemento);
