d14e216c3cfce9b5b45ec7b36fa8166a79693ce6
Cleidson Fias <cleidson.fias@hotmail.com>
2023-06-04 12:30:29 -0300

    styles JS

---
+++ JS NAVEGADOR/14_mudando_estilos/scripts.js
@@ -0,0 +1,7 @@
+let elemento = document.querySelector('#t1');
+
+setTimeout(function() {
+    elemento.style.color = 'white';
+    elemento.style.backgroundColor = 'grey';
+    elemento.style.width = '500px'
+}, 1000);
