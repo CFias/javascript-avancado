30672528809afc8de7393b12e695055c2ecd9c22
Cleidson Fias <cleidson.fias@hotmail.com>
2023-06-04 12:30:04 -0300

    All Select

---
+++ JS NAVEGADOR/15_selecionar_varios/scripts.js
@@ -0,0 +1,9 @@
+let item = document.querySelectorAll('.itens-vermelhos');
+
+let item2 = document.querySelectorAll('.itens-azuis');
+
+console.log(item[0]);
+item[0].style.color = 'red';
+console.log(item[0].style.color);
+
+console.log(item2);
