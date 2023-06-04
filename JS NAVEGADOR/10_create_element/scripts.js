beb720db407c1acf7019841beb355c29c91f7342
Cleidson Fias <cleidson.fias@hotmail.com>
2023-06-04 00:17:44 -0300

    createElement

---
+++ JS NAVEGADOR/10_create_element/scripts.js
@@ -0,0 +1,17 @@
+let lista = document.createElement('ul');
+
+for(let i = 0; i < 5; i++) {
+
+    let item = document.createElement('li');
+
+    let texto = document.createTextNode("Texto lista " + i);
+
+    item.appendChild(texto);
+
+    lista.appendChild(item);
+
+};
+
+let container = document.getElementById('container-principal');
+
+container.appendChild(lista);
